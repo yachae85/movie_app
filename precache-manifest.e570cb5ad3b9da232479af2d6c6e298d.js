c4b2dd512edf093aee623f37c9d64880b28f6f55
ungs <yachae85@gmail.com>
2020-08-17 19:09:46 +0900

    Updates

---
+++ precache-manifest.e570cb5ad3b9da232479af2d6c6e298d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e477b39d07243fe9d529d5f37b59fcf2",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a1b0ff96f9b8a3e05e0c",
+    "url": "/movie_app/static/css/main.c960f81b.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b0ff96f9b8a3e05e0c",
+    "url": "/movie_app/static/js/main.4cc3264c.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
